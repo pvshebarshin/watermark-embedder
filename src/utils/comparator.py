4203f04948696cbb9857870a5328f2de9a5dbb59
pvshebarshin <pvshebarshin@edu.hse.ru>
2024-03-20 22:02:02 +0300

    Update comparator and add test for it

--- src/utils/comparator.py
+++ src/utils/comparator.py
@@ -10,6 +10,15 @@ K_PSNR = 0.4
 K_C = 0.1
 
 
+def count_different_cells(matrix1, matrix2):
+    count = 0
+    for i in range(len(matrix1)):
+        for j in range(len(matrix1[0])):
+            if matrix1[i][j] != matrix2[i][j]:
+                count += 1
+    return count
+
+
 def mse(I, S):
     if I.shape != S.shape:
         raise ValueError("Изображения должны иметь одинаковый размер.")
@@ -24,8 +33,12 @@ def getPSNR_f(block_before, block_after):
     _mse = mse(block_before, block_after)
     if _mse == 0:
         return 0
-    psnr_block = 10 * math.log10((255**2) / _mse)
+    psnr_block = 10 * math.log10((255 ** 2) / _mse)
     return psnr_block / PSNR_MAX
+
+
+def getCapacity_f(block_before, block_after):
+    return count_different_cells(block_before, block_after) / C_MAX
 
 
 def optimization_function(block_before, block_after):
@@ -34,21 +47,3 @@ def optimization_function(block_before, block_after):
             + K_PSNR * getPSNR_f(block_before, block_after)
             + K_E * getErrors(block_before, block_after)
     )
-
-
-def correlation(image1, image2):
-    return signal.correlate2d(image1, image2)
-
-
-def meanSquareError(image1, image2):
-    error = np.sum((image1.astype('float') - image2.astype('float')) ** 2)
-    error /= float(image1.shape[0] * image1.shape[1])
-    return error
-
-
-def psnr(image1, image2):
-    _mse = meanSquareError(image1, image2)
-    if _mse == 0:
-        return 100
-    pixel_max = 255.0
-    return 20 * math.log10(pixel_max / math.sqrt(_mse))
