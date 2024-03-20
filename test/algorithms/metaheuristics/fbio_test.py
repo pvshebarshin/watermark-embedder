2662faa4a87e2d25f97133717d4b44753dcbd890
pvshebarshin <pvshebarshin@edu.hse.ru>
2024-03-20 18:32:10 +0300

    Add FBIO

---
+++ test/algorithms/metaheuristics/fbio_test.py
@@ -0,0 +1,25 @@
+import unittest
+
+from algorithms.metaheuristics.fbio import FBIO
+from utils.functions import eggholder_function
+
+
+class FbioTest(unittest.TestCase):
+    # -959.6407
+    problem_eggholder = {
+        "fit_func": eggholder_function,
+        "lb": [-512, -512],
+        "ub": [512, 512],
+        "minmax": "min",
+        "log_to": None,
+        "save_population": False,
+    }
+
+    def test_fbio(self):
+        epoch = 150
+        pop_size = 50
+
+        # FBIO
+        model = FBIO(epoch, pop_size)
+        best_position, best_fitness = model.solve(FbioTest.problem_eggholder)
+        self.assertTrue(best_fitness < -958)
